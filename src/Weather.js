b1afe830d25bd7800add79b52f6f7e60d21d9031
isataguchi <122779306+isataguchi@users.noreply.github.com>
2023-02-28 01:41:48 +0900

    add API

--- src/Weather.js
+++ src/Weather.js
@@ -1,58 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="WeatherInfo">
-        <h1>New York</h1>
-        <ul>
-          <li>Wednesday, 00:57</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="d-flex">
-              <div>
-                <img
-                  src="http://openweathermap.org/img/wn/02d@2x.png"
-                  alt="few clouds"
-                />
-              </div>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
-              <div>
-                <span className="temperature">6</span>
-                <span className="unit">°C</span>
-              </div>
+  function handleResponse(response) {
+    setReady(true);
+    setWeatherData({
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      temperature: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.main.humidity,
+      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Description: Partly cloud</li>
-              <li>Humidity: 90%</li>
-              <li>Wind: 2 km/h</li>
-            </ul>
+        </form>
+        <div className="WeatherInfo">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>Wednesday, 00:57</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+          </ul>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="d-flex">
+                <div>
+                  <img
+                    src={weatherData.iconURL}
+                    alt={weatherData.description}
+                  />
+                </div>
+
+                <div>
+                  <span className="temperature">{weatherData.temperature}</span>
+                  <span className="unit">°C</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "New York";
+    const apiKey = "dd7f66bea7013249d9e9080f6245905a";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(handleResponse);
+    return "Loading...";
+  }
 }
